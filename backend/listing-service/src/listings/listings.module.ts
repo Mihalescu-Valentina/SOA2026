a4f61ed3fc59b0184f1747ccbc6e4a7b021ee972
Mihalescu Valentina <116385456+Mihalescu-Valentina@users.noreply.github.com>
2026-01-28 17:20:14 +0200

    Add Kafka service to ListingsModule

--- backend/listing-service/src/listings/listings.module.ts
+++ backend/listing-service/src/listings/listings.module.ts
@@ -6,6 +6,7 @@ import { ListingsController } from './listings.controller';
 import { Listing } from './entities/listing.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ListingsGateway } from './listings.gateway';
 
 @Module({
   imports: [
@@ -17,16 +18,28 @@ import { JwtStrategy } from './jwt.strategy';
         name: 'TRANSACTION_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://localhost:5672'],
+          urls: [process.env.RABBITMQ_URL || 'amqp://rabbitmq:5672'],
           queue: 'transactions_queue',
           queueOptions: {
             durable: false,
           },
         },
       },
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: [process.env.KAFKA_BROKER || 'localhost:9092'],
+          },
+          consumer: {
+            groupId: 'listing-producer',
+          },
+        },
+      },
     ]),
   ],
   controllers: [ListingsController],
-  providers: [ListingsService, JwtStrategy],
+  providers: [ListingsService, JwtStrategy, ListingsGateway],
 })
-export class ListingsModule {}
+export class ListingsModule { }
